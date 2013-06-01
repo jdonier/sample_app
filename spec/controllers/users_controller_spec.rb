91b15b99fe18d41efd0ed8703b4d06a640c4428c
Jonathan Donier <jonathan.donier@gmail.com>
2013-06-01 23:47:48 +0200

    mdp et profil

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,49 @@
 ﻿require 'spec_helper'
 
 describe UsersController do
-render_views
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-    it "devrait avoir le titre adéquat" do
-      get 'new'
-      response.should have_selector("title", :content => "Inscription")
-    end
-  end
+
+	render_views
+
+	  describe "GET 'show'" do
+
+		before(:each) do
+		  @user = Factory(:user)
+		end
+
+		it "devrait réussir" do
+		  get :show, :id => @user
+		  response.should be_success
+		end
+
+		it "devrait trouver le bon utilisateur" do
+		  get :show, :id => @user
+		  assigns(:user).should == @user
+		end
+		it "devrait avoir le bon titre" do
+		  get :show, :id => @user
+		  response.should have_selector("title", :content => @user.nom)
+		end
+
+		it "devrait inclure le nom de l'utilisateur" do
+		  get :show, :id => @user
+		  response.should have_selector("h1", :content => @user.nom)
+		end
+
+		it "devrait avoir une image de profil" do
+		  get :show, :id => @user
+		  response.should have_selector("h1>img", :class => "gravatar")
+		end
+	  end
+	  
+	  describe "GET 'new'" do
+		it "returns http success" do
+		  get 'new'
+		  response.should be_success
+		end
+		it "devrait avoir le titre adéquat" do
+		  get 'new'
+		  response.should have_selector("title", :content => "Inscription")
+		end
+	  end
 
 end
