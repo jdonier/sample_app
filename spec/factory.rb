2568d460b35a9dfef35eee410e81ef8bcca64665
Jonathan Donier <jonathan.donier@gmail.com>
2013-06-02 11:04:23 +0200

    signup

---
+++ spec/factory.rb
@@ -0,0 +1,9 @@
+﻿# En utilisant le symbole ':user', nous faisons que
+# Factory Girl simule un modèle User.
+
+FactoryGirl.define :user do |user|
+	user.nom "Jonathan Donier"
+	user.email "jdonier@example.com"
+	user.password "foobar"
+	user.password_confirmation "foobar"
+end	
